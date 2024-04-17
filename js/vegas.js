32c152ea38e86d22db9f67f3ef78d9448156a8bc
kuuna908 <167284477+kuuna908@users.noreply.github.com>
2024-04-17 15:58:54 +0900

    Add files via upload

---
+++ js/vegas.js
@@ -0,0 +1,31 @@
+$(function() {
+    $('.mainimg-tate').vegas({
+        slides: [
+            { src: 'images/IMG_4175.jpeg' },
+            { src: 'images/IMG_4822.jpeg' },	//1枚目の写真指定
+            { src: 'images/IMG_6382.jpeg' },	//2枚目の写真指定
+            { src: 'images/IMG_7043.jpeg'}	,
+        ],
+		transition: 'blur',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
+		animation: 'kenburns',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
+		delay: 6000,				//次の画像を表示するまでの時間
+		animationDuration: 10000,	//アニメーション間の引き継ぎタイミング。
+		timer: false,				//プログレスバーを非表示に。
+    });
+});
+
+//なくても⚪︎つけるならHTML編集
+$(function() {
+    $('.mainimg-yoko').vegas({
+        slides: [
+            { src: './images/yoko_1.jpg' },	//1枚目の写真指定
+            { src: './images/yoko_2.jpg' },	//2枚目の写真指定
+            { src: './images/yoko_3.jpg' },	//3枚目の写真指定
+        ],
+		transition: 'blur',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
+		animation: 'kenburns',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
+		delay: 6000,				//次の画像を表示するまでの時間
+		animationDuration: 10000,	//アニメーション間の引き継ぎタイミング。
+		timer: false,				//プログレスバーを非表示に。
+    });
+});
